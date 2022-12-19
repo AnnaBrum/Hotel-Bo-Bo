8a2c12b9dc30f70e32b3b8b1c561ce393449bd34
AnnaBrum <annahelenabrumark@gmail.com>
2022-12-19 15:56:54 +0100

    Connected form to database
    - created connection between form and database.
    - values inserted in form are saved to database.

---
+++ dbconnection.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+require __DIR__ . '/hotelFunctions.php';
+
+$db = connect('/yrgopelago.db');
+$statement = $db->query('SELECT * FROM bookings');
+$bookings = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_POST['name'], $_POST['transfer-code'], $_POST['arrival'], $_POST['departure'], $_POST['room'])) {
+
+    $name = htmlspecialchars($_POST['name'],ENT_QUOTES);
+    $transferCode = trim(htmlspecialchars($_POST['transfer-code'],ENT_QUOTES));
+    $arrival = trim(htmlspecialchars($_POST['arrival'],ENT_QUOTES));
+    $departure = trim(htmlspecialchars($_POST['departure'],ENT_QUOTES));
+    $room = trim(htmlspecialchars($_POST['room'],ENT_QUOTES));
+
+    // if arrivaldate is between x and x 
+
+    $query = 'INSERT INTO bookings (
+        name,
+        transfer_code,
+        arrival_date,
+        departure_date,
+        room_id
+        )
+        VALUES (
+            :name,
+            :transfer_code,
+            :arrival_date,
+            :departure_date,
+            :room_id
+        )';
+
+    $statement = $db->prepare($query);
+
+    $statement->bindParam(':name', $name, PDO::PARAM_STR);
+    $statement->bindParam(':transfer_code', $transferCode, PDO::PARAM_STR);
+    $statement->bindParam(':arrival_date', $arrival, PDO::PARAM_STR);
+    $statement->bindParam(':departure_date', $departure, PDO::PARAM_STR);
+    $statement->bindParam(':room_id', $room, PDO::PARAM_STR);
+
+    $statement->execute();
+
+}
+
+
+
+// print_r($bookings);
--- index.php
+++ index.php
@@ -1,96 +1,123 @@
 <?php
+
 declare(strict_types=1);
-include_once 'calendar.php';
+include_once __DIR__ . '/calendar.php';
+require __DIR__ . '/dbconnection.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" type="text/css" href="/calendar.css">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" type="text/css" href="/calendar.css">
 
 
-    <title>Bates motel</title>
-  </head>
+  <title>Bates motel</title>
+</head>
 
-  <body>
-    <main>
-      <section class="hero">
-        <div class="hero-image">
-          <!-- <img src="/images/hero.jpeg" alt="Bates Motel sign" srcset="" /> -->
-        </div>
-        <article class="welcoming">
-          <h1>Welcome to Bates Motel!</h1>
-          <p>
-            This cozy family run gem of a motel won't let you down with it's warm hospitality and care for details. Your host Norman Bates lives in the family-villa just a stone's throw from the main facilities and will be at your service before you even know you need it!
-          </p>
-        </article>
-      </section>
+<body>
+  <main>
+    <section class="hero">
+      <div class="hero-image">
+        <!-- <img src="/images/hero.jpeg" alt="Bates Motel sign" srcset="" /> -->
+      </div>
+      <article class="welcoming">
+        <h1>Welcome to Bates Motel!</h1>
+        <p>
+          This cozy family run gem of a motel won't let you down with it's warm hospitality and care for details. Your host Norman Bates lives in the family-villa just a stone's throw from the main facilities and will be at your service before you even know you need it!
+        </p>
+      </article>
+    </section>
 
-      <section class="rooms">
-        <article class="economy">
-          <h2>ECONOMY</h2>
-          <div class="room-wrapper">
-            <img
-              class="room-img"
-              src="./images/economy.jpg"
-              alt="economy motel-room"
-            />
-            <p class="room-description">
+    <section class="rooms">
+      <article class="economy">
+        <h2>ECONOMY</h2>
+        <div class="room-wrapper">
+          <img class="room-img" src="./images/economy.jpg" alt="economy motel-room" />
+          <p class="room-description">
             consectetur adipisicing elit.
-              Consequuntur ex iure dolorum maiores nihil ipsam ipsum hic autem
-              ea consequatur, non dolor asperiores adipisci assumenda
-              dignissimos? Nisi tenetur molestias dolores.
-            </p>
-          </div>
-          <h3>VACANCIES</h3>
-          <div class="calendar">
-            <?=$calendar->draw(date('2023-01-01'));?>
-          </div>
-        </article>
+            Consequuntur ex iure dolorum maiores nihil ipsam ipsum hic autem
+            ea consequatur, non dolor asperiores adipisci assumenda
+            dignissimos? Nisi tenetur molestias dolores.
+          </p>
+        </div>
+        <h3>VACANCIES</h3>
+        <div class="calendar">
+          <?= $calendar->draw(date('2023-01-01')); ?>
+        </div>
+      </article>
 
-        <article class="standard">
-          <h2>STANDARD</h2>
-          <div class="room-wrapper">
-            <img
-              class="room-img"
-              src="./images/standard.jpeg"
-              alt="standard motel-room"
-            />
-            <p class="room-description">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consequuntur ex iure dolorum maiores nihil ipsam ipsum hic autem
-              ea consequatur, non dolor asperiores adipisci assumenda
-              dignissimos? Nisi tenetur molestias dolores.
-            </p>
-          </div>
-          <h3>VACANCIES</h3>
-          <div class="calendar"><?=$calendar->draw(date('2023-01-01'));?></div>
-        </article>
+      <article class="standard">
+        <h2>STANDARD</h2>
+        <div class="room-wrapper">
+          <img class="room-img" src="./images/standard.jpeg" alt="standard motel-room" />
+          <p class="room-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequuntur ex iure dolorum maiores nihil ipsam ipsum hic autem
+            ea consequatur, non dolor asperiores adipisci assumenda
+            dignissimos? Nisi tenetur molestias dolores.
+          </p>
+        </div>
+        <h3>VACANCIES</h3>
+        <div class="calendar"><?= $calendar->draw(date('2023-01-01')); ?></div>
+      </article>
 
-        <article class="luxury">
-          <h2>LUXURY</h2>
-          <div class="room-wrapper">
-            <img
-              class="room-img"
-              src="./images/luxury.jpeg"
-              alt="luxury motel-room"
-            />
-            <p class="room-description">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consequuntur ex iure dolorum maiores nihil ipsam ipsum hic autem
-              ea consequatur, non dolor asperiores adipisci assumenda
-              dignissimos? Nisi tenetur molestias dolores.
-            </p>
-          </div>
-          <h3>VACANCIES</h3>
-          <div class="calendar"><?=$calendar->draw(date('2023-01-01'));?></div>
-        </article>
-      </section>
-    </main>
-  </body>
+      <article class="luxury">
+        <h2>LUXURY</h2>
+        <div class="room-wrapper">
+          <img class="room-img" src="./images/luxury.jpeg" alt="luxury motel-room" />
+          <p class="room-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequuntur ex iure dolorum maiores nihil ipsam ipsum hic autem
+            ea consequatur, non dolor asperiores adipisci assumenda
+            dignissimos? Nisi tenetur molestias dolores.
+          </p>
+        </div>
+        <h3>VACANCIES</h3>
+        <div class="calendar"><?= $calendar->draw(date('2023-01-01')); ?></div>
+      </article>
+    </section>
+
+    <section class="booking-form">
+      <div class="booking-wrapper">
+        <form action="/index.php" method="post">
+          <!-- transfer-code -->
+          <label for="name">Your Name</label>
+          <input type="text" name="name">
+          <!-- transfer-code -->
+          <label for="transfer-code">Transfer-code</label>
+          <input type="text" name="transfer-code">
+          <!-- arrival-date -->
+          <label for="arrival" class="form-input">Arrival</label>
+          <input type="date" name="arrival" class="form-input">
+          <!-- departure-date -->
+          <label for="departure">Departure</label>
+          <input type="date" name="departure" class="form-input">
+          <!-- choose room -->
+          <label for="room">Choose Room</label>
+          <select name="room" class="form-input">
+            <option value="1">Budget</option>
+            <option value="2">Standard</option>
+            <option value="3">Luxury</option>
+          </select><br>
+          <!-- features -->
+          <!-- <input type="radio" id="html" name="fav_language" value="HTML">
+          <label for="html">HTML</label><br>
+          <input type="radio" id="css" name="fav_language" value="CSS">
+          <label for="css">CSS</label><br>
+          <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+          <label for="javascript">JavaScript</label> -->
+          <!-- submit -->
+          <button type="submit">Make a reservation</button>
+        </form>
+      </div>
+    </section>
+  </main>
+</body>
+
 </html>
